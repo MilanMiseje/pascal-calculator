d5c6f40ab2945a67582c18caa1bd96ea91f6983b
Milan Miseje <milan.miseje@gmail.com>
2026-09-13 19:22:31 +0200

    Implement Calculator operations

--- src/Calculator.pas
+++ src/Calculator.pas
@@ -20,31 +20,40 @@ implementation
 // Function to perform addition
 function Add(num1, num2: Double): Double;
 begin
-    Result := 0;
+    Result := num1 + num2;
 end;
 
 // Function to perform subtraction
 function Subtract(num1, num2: Double): Double;
 begin
-    Result := 0;
+    Result := num1 - num2;
 end;
 
 // Function to perform multiplication
 function Multiply(num1, num2: Double): Double;
 begin
-    Result := 0;
+    Result := num1 * num2;
 end;
 
 // Function to perform division
 function Divide(num1, num2: Double): Double;
 begin
-    Result := 0;
+    if num2 = 0 then
+        raise EDivisionByZero.Create('Division by zero is not allowed.');
+    Result := num1 / num2;
 end;
 
 // Function to perform the calculation based on the operation
 function Calculate(num1, num2: Double; operation: Char): Double;
 begin
-    Result := 0;
+    case operation of
+        '+': Result := Add(num1, num2);
+        '-': Result := Subtract(num1, num2);
+        '*': Result := Multiply(num1, num2);
+        '/': Result := Divide(num1, num2);
+    else
+        raise EInvalidOperation.Create('Invalid operation. Please use +, -, *, or /.');
+    end;
 end;
 
 end.
